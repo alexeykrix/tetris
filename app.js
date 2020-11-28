525c8fd2dbc64629b49db7efc45d48ad5e308e67
alexeykrix <alexey.2k04@gmail.com>
2020-11-28 23:51:26 +0200

    init

--- app.js
+++ app.js
@@ -1,330 +1,438 @@
 const btnPlay = document.querySelector('.play')
 const btnPause = document.querySelector('.pause')
 const btnReset = document.querySelector('.reset')
-const arrows = document.querySelector('.arrows')
 const range = document.querySelectorAll('.size')
 const cellRange = document.querySelectorAll('.cellSize')
 const canvas = document.querySelector('#canvas')
 const c = canvas.getContext('2d')
-canvas.width = canvas.height = localStorage.getItem('width') || 500
+const canvasInfo = document.querySelector('#info')
+const cInfo = canvasInfo.getContext('2d')
+canvas.width = 300
+canvas.height = 600
+canvasInfo.height = 40
 
-range[1].value = range[0].value = localStorage.getItem('width') || 500
-cellRange[1].value = cellRange[0].value = localStorage.getItem('cellSize') || 10
+c.fillStyle = cInfo.fillStyle  = '#eeeeee'
+c.strokeStyle = cInfo.strokeStyle  = '#000'
+c.lineWidth = cInfo.lineWidth  = '1'
+let score = 0
 
-c.fillStyle = '#eeeeee'
-c.strokeStyle = '#000'
-c.lineWidth = '1'
-let score = 5
+let cellSize = 30
+let cellsX = canvas.width / cellSize
+let cellsY = canvas.height / cellSize
 
-let cellSize = localStorage.getItem('cellSize') || 10
-let cellsCount = canvas.width / cellSize
+const figures = [
+  [ //  I
+    [{x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3},],
+    [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1},],
+    [{x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3},],
+    [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1},]
+  ],
+  [ // J
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 0, y: 2},],
+    [{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1},],
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 2, y: 0},],
+    [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 2, y: 2},]
+  ],
+  [ // L
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 2, y: 2},],
+    [{x: 0, y: 2}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1},],
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 0, y: 0},],
+    [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 2, y: 0},]
+  ],
+  [ // O
+    [{x: 1, y: 0}, {x: 2, y: 0}, {x: 1, y: 1}, {x: 2, y: 1},],
+    [{x: 1, y: 0}, {x: 2, y: 0}, {x: 1, y: 1}, {x: 2, y: 1},],
+    [{x: 1, y: 0}, {x: 2, y: 0}, {x: 1, y: 1}, {x: 2, y: 1},],
+    [{x: 1, y: 0}, {x: 2, y: 0}, {x: 1, y: 1}, {x: 2, y: 1},]
+  ],
+  [ // S
+    [{x: 1, y: 0}, {x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1},],
+    [{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 1, y: 2},],
+    [{x: 1, y: 0}, {x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1},],
+    [{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 1, y: 2},],
+  ],
+  [ // T
+    [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 1, y: 2},],
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 0, y: 1},],
+    [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 1, y: 0},],
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 2, y: 1},],
+  ],
+  [ // Z
+    [{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}, ],
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 0, y: 1}, {x: 0, y: 2}, ],
+    [{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}, ],
+    [{x: 1, y: 0}, {x: 1, y: 1}, {x: 0, y: 1}, {x: 0, y: 2}, ],
+  ],
+]
 let gameData = []
-let direction = ''
-let food;
+let figId
+let figure, current = []
+let figRotate = 0
 
-const randomPos = () => {
-  let rand = - 0.5 + Math.random() * cellsCount;
+let scoreMylty = 0
+let speed = 1000/(score/10+1.5)
+
+const randomFigure = () => {
+  let rand = - 0.5 + Math.random() * figures.length;
   return Math.round(rand);
 }
-const randFoodPos = () => {
-  food = {
-    'x': randomPos(),
-    'y': randomPos(),
-    'state': 0,
+const checkSolved = () => {
+  for (let i = 0; i < 20; i++) {
+    lastRow = [0,0,0,0,0,0,0,0,0,0,]
+    gameData.forEach(el=> {
+      if (el.y === i) {
+        lastRow[el.x] = 1
+      }
+    })
+  
+    allLine = true
+    lastRow.forEach(el => el === 1? '': allLine=false)
+
+    let ids = []
+    
+    if (allLine === true) {
+      gameData.forEach((el, id) => el.y === i? ids.push(id) :'')
+      gameData.forEach(el => el.y <= i? el.y++ :'')
+    }
+
+    ids.forEach(id => {
+      gameData[id].x = -1
+      gameData[id].y = -1
+    })
+
+    gameData = gameData.filter(a => (a.x !== -1 && a.y !== -1))
+
+    if (ids.length) scoreMylty++
+    
   }
-  gameData.forEach(el => {
-    if (el.x === food.x && el.y === food.y) randFoodPos()
-  })
 }
 
-const makeArray = () => {
-  gameData = []
-  for (let i=0; i < 5; i++) {
-    gameData.push({'x': Math.round(cellsCount/2), 'y': Math.round(cellsCount/2), state: 1})
-  }
+
+let nextFigureId
+nextFigureId = randomFigure()
+
+const nextFig = (isInit) => {
+  isInit? '' : current.forEach(el => gameData.push(JSON.parse(JSON.stringify(el))))
+  let colors = ['#e67e22','#e74c3c','#3498db','#9b59b6','#f1c40f',
+      '#2ecc71','#44bd32','#f39c12','#16a085','#c0392b','#2980b9',
+      '#8e44ad','#575fcf','#e1b12c','#ff5e57','#cf6a87','#f8a5c2',
+      '#f7d794','#f3a683','#786fa6','#e84118',]
+  let color = colors[Math.round(-0.5 + Math.random()*colors.length)]
+  figId = nextFigureId
+  figure = JSON.parse(JSON.stringify(figures[figId]))
+  current = figure[figRotate].slice()
+  current.forEach(el => el.color = color)
+  current.forEach(el=> el.x = el.x +3)
+  checkSolved()
+  nextFigureId = randomFigure()
+  
 }
 
-const renderCell = (x, y, state, id) => {
-  state? c.fillStyle = 
-    (id === 0 ? '#e74c3c' : 
-      (id%2 === 0 ? '#f39c12' : '#e67e22')): c.fillStyle = '#64dd17'
+
+const clearCanvas = () => {
+  c.fillStyle = '#3d3d3e'
+  c.beginPath()
+  c.rect(0, 0, canvas.width, canvas.height)
+  c.fill()
+  c.closePath()
+
+  cInfo.fillStyle = '#3d3d3e'
+  cInfo.beginPath()
+  cInfo.rect(0, 0, canvas.width, canvas.height)
+  cInfo.fill()
+  cInfo.closePath()
+}
+
+const renderBlock = (x, y, color) => {  
+  c.fillStyle = color
+
   c.beginPath()
   c.rect(x*cellSize, y*cellSize, cellSize, cellSize)
   c.fill()
   c.closePath()
 }
 
-const renderCells = () => {
-  c.fillStyle = '#3d3d3e'
-  c.beginPath()
-  c.rect(0, 0, canvas.width, canvas.width)
-  c.fill()
-  c.closePath()
+const renderNextBlock = (x, y, color) => {  
+  cInfo.fillStyle = color
+
+  cInfo.beginPath()
+  cInfo.rect(x*10+10, y*10, 10, 10)
+  cInfo.fill()
+  cInfo.closePath()
+}
+
+const render = () => {
+  clearCanvas()
+
+  cInfo.fillStyle = '#e67e22'
+  cInfo.font = '20px "Roboto Mono"'
+  cInfo.textAlign = 'right';
+  cInfo.fillText('best: '+ (localStorage.getItem('bestTetris') || 0), 290, 27)
+  
   c.fillStyle = '#242424'
   c.font = '90px "Roboto Mono"'
   c.textAlign = 'center';
   c.fillText(score, canvas.width/2, canvas.width/2+20)
-  renderCell(food.x, food.y, 0)
-  gameData.forEach((el, id) => {
-    renderCell(el.x, el.y, el.state, id)
+  
+  figures[nextFigureId][0]
+    .forEach( block => renderNextBlock(block.x, block.y, '#e67e22'))
+
+  let fallFigure = [0,0,0,0,0,0,0,0,0,0]
+  current.forEach(block=> {
+    if (fallFigure[block.x] < block.y) {
+      fallFigure[block.x] = block.y
+    }
   })
+
+  fallFigure.forEach((y, x) => {
+    if (y > 0) {
+      for (let i=1; i < 20; i++) {
+        renderBlock(x, y+i, '#12121240')
+      }
+    }
+  })
+
+  current.forEach( block => renderBlock(block.x, block.y, block.color))
+  gameData.forEach( block => renderBlock(block.x, block.y, block.color))
 }
+
+const moveFigure = () => {
+  let scoreset = [0, 1, 3, 7, 15]
+
+  if (scoreMylty > 0) {
+    clearInterval(moveTimeout)
+    moveTimeout = setInterval(() =>moveFigure(), speed)
+  } 
+  score += scoreset[scoreMylty]
+  scoreMylty = 0
+
+  if (+localStorage.getItem('bestTetris') < score) {
+    localStorage.setItem('bestTetris', score)
+  }
+
+  let fallFigure = [0,0,0,0,0,0,0,0,0,0]
+  current.forEach(block=> {
+    if (fallFigure[block.x] < block.y) {
+      fallFigure[block.x] = block.y
+    }
+  })
+
+  let futurePos = []
+  let isNewBusy = false
+
+  fallFigure.forEach( (el, id) => {
+    if (el !== 0) futurePos.push({'x': id, 'y': el+1})
+  })
+  
+  futurePos.forEach( el => {
+    if (19 < el.y ) isNewBusy = true
+  })
+
+  for (let i = 0; i < gameData.length-1; i++) {
+    let data = gameData[i]
+    futurePos.forEach( el => {
+      if (data.x === el.x && data.y === el.y ) isNewBusy = true
+    })
+  }
+
+  if (isNewBusy) {
+    if (current[0].y === 0 ||
+        current[0].y === 1 ||
+        current[0].y === 2) {
+      score = 0
+      clearInterval(renderTimeout)
+      clearInterval(moveTimeout)
+      renderTimeout = undefined
+      moveTimeout = undefined
+    
+      c.fillStyle = '#121212e0'
+      c.beginPath()
+      c.rect(0, 0, canvas.width, canvas.height)
+      c.fill()
+      c.closePath()
+    
+      c.fillStyle = '#ededed'
+      c.font = '50px "Roboto Mono"'
+      c.textAlign = 'center';
+      c.fillText('GAME OVER', canvas.width/2, canvas.height/2 +25)
+      
+      gameData = []
+      return
+    }
+
+    nextFig()
+  } else current.forEach(block => block.y++)
+  
+}
+
+let renderTimeout
+let moveTimeout
 
 const init = () => {
-  makeArray(cellsCount, cellsCount)
-  randFoodPos()
-
-  c.fillStyle = '#2e2d2e'
-  c.beginPath()
-  c.rect(0,0,c.width,c.height)
-  c.closePath()
-  c.fill()
-
-  renderCells()
+  nextFig(true)
+  renderTimeout = setInterval(() =>render(), 20)
+  moveTimeout = setInterval(() =>moveFigure(), speed)
+  render()
 }
-
 init()
 
-const selfEating = (id) => {
-  gameData = gameData.slice(0, id)
+const rotateHandler = () => {
+  let nextFigId = figRotate !== 3? figRotate +1 : 0
+  let thisRotate = figures[figId][figRotate]
+  let nextRotate = figures[figId][nextFigId]
+  let moveX, moveY
+  let isNewBusy = false
+  let movedFig = []
+  current.forEach( block => movedFig.push(JSON.parse(JSON.stringify(block))))
+
+  moveX = movedFig[0].x - thisRotate[0].x
+  moveY = movedFig[0].y - thisRotate[0].y
+
+  nextRotate.forEach( el => {
+    if (9 < (el.x + moveX) || 0 > (el.x + moveX) || 
+        19 < (el.y + moveY) ) isNewBusy = true
+  })
+
+  for (let i = 0; i < gameData.length-1; i++) {
+    let data = gameData[i]
+    nextRotate.forEach( el => {
+      if (data.x === (el.x + moveX) && 
+          data.y === (el.y + moveY) ) isNewBusy = true
+    })
+  }
+
+  if (!isNewBusy) {
+    current.forEach( (block, id) => {
+      block.x = nextRotate[id].x + moveX
+      block.y = nextRotate[id].y + moveY
+    })  
+    figRotate = nextFigId
+  }
 }
 
-const snakeMoving = () => {
-  let d = direction
-  let eatId;
-  const oldData = [...gameData]
+const leftHandler = () => {
+  let fallFigure = [10,10,10,10,10,10,10,10,10,10,
+                    10,10,10,10,10,10,10,10,10,10]
   
-  if (d === 'left') { // left
-    let next = oldData[0].x-1 >= 0 ? oldData[0].x-1 : cellsCount-1
-    oldData.forEach((el, id) => {
-      if (el.x === next && 
-          el.y === oldData[0].y) {
-      eatId = id || ''
+  let isNewBusy = false
+  current.forEach(block=> {
+    if (fallFigure[block.y] > block.x) fallFigure[block.y] = block.x
+  })
+  let fallFigureY = 20
+  fallFigure = fallFigure.filter(a => {
+    if (a <= 9) {
+      if (a-1 < 0) isNewBusy = true
+      if (fallFigureY > fallFigure.indexOf(a)) {
+        fallFigureY = fallFigure.indexOf(a)
       }
-    })
-    if (eatId) selfEating(eatId)
-    gameData.unshift({
-      'x': next,
-      'y': gameData[0].y,
-      'state': 1
-    })
-    if (food.x === next && 
-        food.y === gameData[0].y) {
-      randFoodPos()
-      return
-    } else {
-      gameData.pop()
+      return true
     }
+  })
+  
+  let futurePos = []
+  fallFigure.forEach((x, id) => {
+    futurePos.push({
+      'x': x-1,
+      'y': fallFigureY + id,
+    })
+  })
+
+  for (let i = 0; i < gameData.length-1; i++) {
+    let data = gameData[i]
+    futurePos.forEach( el => {
+      if (data.x === el.x && data.y === el.y ) isNewBusy = true
+    })
   }
-  if (d === 'up') { // up
-    const oldData = [...gameData]
-    let next = oldData[0].y-1 >= 0 ? oldData[0].y-1 : cellsCount-1
-    oldData.forEach((el, id) => {
-      if (el.x === oldData[0].x && 
-          el.y === next) {
-      eatId = id || ''
-      }
-    })
-    if (eatId) selfEating(eatId)
-    gameData.unshift({
-      'x': gameData[0].x,
-      'y': next,
-      'state': 1
-    })
-    if (food.x === gameData[0].x && 
-        food.y === next) {
-      randFoodPos()
-      return
-    } else {
-      gameData.pop()
-    }
-  }
-  if (d === 'down') { // down
-    const oldData = [...gameData]
-    let next = oldData[0].y+1 < cellsCount ? oldData[0].y+1 : 0
-    oldData.forEach((el, id) => {
-      if (el.x === oldData[0].x && 
-          el.y === next) {
-        eatId = id || ''
-      }
-    })
-    if (eatId) selfEating(eatId)
-    gameData.unshift({
-      'x': gameData[0].x,
-      'y': next,
-      'state': 1
-    })
-    if (food.x === gameData[0].x && 
-        food.y === next) {
-      randFoodPos()
-      return
-    } else {
-      gameData.pop()
-    }
-  }
-  if (d === 'right') { // right
-    const oldData = [...gameData]
-    let next = oldData[0].x+1 < cellsCount ? oldData[0].x+1 : 0
-    oldData.forEach((el, id) => {
-      if (el.x === next && 
-          el.y === oldData[0].y) {
-        eatId = id || ''
-      }
-    })
-    if (eatId) selfEating(eatId)
-    gameData.unshift({
-      'x': next,
-      'y': gameData[0].y,
-      'state': 1
-    })
-    if (food.x === next && 
-        food.y === gameData[0].y) {
-      
-          randFoodPos()
-      return
-    } else {
-      gameData.pop()
-    }
-  }
-  renderCells()
+
+  !isNewBusy? current.forEach(el => el.x--) :''
 }
 
-const gameRun = () => {
-  snakeMoving()
-  score = gameData.length
+const rightHandler = () => {
+  let fallFigure = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+                    -1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+  let isNewBusy = false
+  current.forEach(block => {
+    if (fallFigure[block.y] < block.x) fallFigure[block.y] = block.x
+  })
+  let fallFigureY = 20
+  fallFigure = fallFigure.filter(a => {
+    if (a > 0 ) {
+      if (a+1 > 9) isNewBusy = true
+      if (fallFigureY > fallFigure.indexOf(a)) {
+        fallFigureY = fallFigure.indexOf(a)
+      }
+      return true
+    }
+  })
+  
+  let futurePos = []
+  fallFigure.forEach((x, id) => {
+    futurePos.push({
+      'x': x+1,
+      'y': fallFigureY + id,
+    })
+  })
+  for (let i = 0; i < gameData.length-1; i++) {
+    let data = gameData[i]
+    futurePos.forEach( el => {
+      if (data.x === el.x && data.y === el.y ) isNewBusy = 1
+    })
+  }
+  !isNewBusy? current.forEach(el => el.x++) :''
 }
 
-let timeout = false
+
+//        events
+
 const keydownHandler = (e) => {
-  if (e.repeat) return
-  if (e.code === 'KeyW' && 
-      direction !== 'down' &&
-      direction !== 'up') direction = 'up'
-  if (e.code === 'KeyA' && 
-      direction !== 'right' &&
-      direction !== 'left') direction = 'left'
-  if (e.code === 'KeyD' && 
-      direction !== 'left' &&
-      direction !== 'right') direction = 'right'
-  if (e.code === 'KeyS' && 
-      direction !== 'up' &&
-      direction !== 'down') direction = 'down'
-
-  if (!timeout) {
-    if ((e.code === 'KeyW') ||
-        (e.code === 'KeyA') ||
-        (e.code === 'KeyD') ||
-        (e.code === 'KeyS')) {
-      timeout = setInterval(() =>gameRun(), 50)
-    }
+  e.preventDefault()
+  if (renderTimeout === undefined && 
+      (e.code === 'Space' || e.code === 'KeyP' ||
+      e.code === 'Enter' || e.code === 'Escape')) {
+    renderTimeout = setInterval(() =>render(), 20)
+    moveTimeout = setInterval(() =>moveFigure(), speed)
   }
+  if (e.code === 'KeyW' || e.code === 'ArrowUp') rotateHandler()
+  if (e.code === 'KeyA' || e.code === 'ArrowLeft') leftHandler()
+  if (e.code === 'KeyD' || e.code === 'ArrowRight') rightHandler()
+  if (e.code === 'KeyS' || e.code === 'ArrowDown') moveFigure()
 }
-
 
 document.addEventListener('keydown', keydownHandler)
 
 btnPause.addEventListener('click', () => {
-  clearInterval(timeout)
-  timeout = false
+  clearInterval(renderTimeout)
+  clearInterval(moveTimeout)
+  renderTimeout = undefined
+  moveTimeout = undefined
+
+  c.fillStyle = '#121212f0'
+  c.beginPath()
+  c.rect(0, 0, canvas.width, canvas.height)
+  c.fill()
+  c.closePath()
 })
 btnReset.addEventListener('click', () => {
-  clearInterval(timeout)
-  timeout = false
+  clearInterval(renderTimeout)
+  clearInterval(moveTimeout)
+  gameData = []
   init()
 })
 
-range[0].addEventListener('change', e => {
-  localStorage.setItem('width', e.target.value)
-  canvas.width = canvas.height = e.target.value
-  cellsCount = canvas.width / cellSize
-  init()
-  range[1].value = e.target.value
-})
-range[1].addEventListener('change', e => {
-  localStorage.setItem('width', e.target.value)
-  canvas.width = canvas.height = e.target.value
-  cellsCount = canvas.width / cellSize
-  init()
-  range[0].value = e.target.value
-})
-
-cellRange[0].addEventListener('change', e => {
-  localStorage.setItem('cellSize', e.target.value)
-  cellSize = e.target.value
-  cellsCount = canvas.width / cellSize
-  init()
-  cellRange[1].value = e.target.value
-})
-cellRange[1].addEventListener('change', e => {
-  localStorage.setItem('cellSize', e.target.value)
-  cellSize = e.target.value
-  cellsCount = canvas.width / cellSize
-  init()
-  cellRange[0].value = e.target.value
-})
-
-arrows.addEventListener('touchstart', evt => {
-  evt.preventDefault()
-  let e = {'code': ''}
+document.addEventListener('touchstart', e => {
+  let t = e.touches[0]
   
-  let x0 = arrows.offsetLeft
-  let y0 = arrows.offsetTop
+  let width = window.innerWidth
+  let height = window.innerHeight
 
-  if (x0<evt.touches[0].clientX+25 &&
-      x0+49>evt.touches[0].clientX+25 &&
-      y0<evt.touches[0].clientY &&
-      y0+49>evt.touches[0].clientY) {
-    e.code = 'KeyW'
-  }
-  if (x0<evt.touches[0].clientX+25 &&
-      x0+49>evt.touches[0].clientX+25 &&
-      y0+99<evt.touches[0].clientY &&
-      y0+149>evt.touches[0].clientY) {
-    e.code = 'KeyS'
-  }
-  if (x0<evt.touches[0].clientX+74 &&
-      x0+49>evt.touches[0].clientX+74 &&
-      y0+49<evt.touches[0].clientY &&
-      y0+99>evt.touches[0].clientY) {
-    e.code = 'KeyA'
-  }
-  if (x0<evt.touches[0].clientX-25 &&
-      x0+49>evt.touches[0].clientX-25 &&
-      y0+49<evt.touches[0].clientY &&
-      y0+99>evt.touches[0].clientY) {
-    e.code = 'KeyD'
-  }
-  keydownHandler(e)
-})
+  if (width/2 > t.clientX && height/2 < t.clientY) leftHandler()
+  if (width/2 < t.clientX && height/2 < t.clientY) rightHandler()
+  if (height/2 > t.clientY) rotateHandler()
+  if (height - 0.8*height < t.clientY) moveFigure()
 
-window.addEventListener('touchmove', evt => {
-  let e = {'code': ''}
-  let x0 = arrows.offsetLeft
-  let y0 = arrows.offsetTop
+  if (renderTimeout === undefined) {
+    renderTimeout = setInterval(() =>render(), 20)
+    moveTimeout = setInterval(() =>moveFigure(), speed)
+  }
 
-  if (x0<evt.touches[0].clientX+25 &&
-      x0+49>evt.touches[0].clientX+25 &&
-      y0<evt.touches[0].clientY &&
-      y0+49>evt.touches[0].clientY) {
-    e.code = 'KeyW'
-  }
-  if (x0<evt.touches[0].clientX+25 &&
-      x0+49>evt.touches[0].clientX+25 &&
-      y0+99<evt.touches[0].clientY &&
-      y0+149>evt.touches[0].clientY) {
-    e.code = 'KeyS'
-  }
-  if (x0<evt.touches[0].clientX+74 &&
-      x0+49>evt.touches[0].clientX+74 &&
-      y0+49<evt.touches[0].clientY &&
-      y0+99>evt.touches[0].clientY) {
-    e.code = 'KeyA'
-  }
-  if (x0<evt.touches[0].clientX-25 &&
-      x0+49>evt.touches[0].clientX-25 &&
-      y0+49<evt.touches[0].clientY &&
-      y0+99>evt.touches[0].clientY) {
-    e.code = 'KeyD'
-  }
-  keydownHandler(e)
 })
